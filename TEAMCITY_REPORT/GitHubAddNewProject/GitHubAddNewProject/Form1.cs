6554f30ea807c5b59e8ebd9f416dbefc2500f34b
Helen <helenwond25@gmail.com>
2017-01-11 11:28:14 -0600

    First git project commint2

--- TEAMCITY_REPORT/GitHubAddNewProject/GitHubAddNewProject/Form1.cs
+++ TEAMCITY_REPORT/GitHubAddNewProject/GitHubAddNewProject/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubAddNewProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
